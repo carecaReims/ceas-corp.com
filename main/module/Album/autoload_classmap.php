7311e546644cf91b5cf21a035f89e17669fe537b
Careca Muanza <c.muanzadiba@ceas-engineering.com>
2016-07-28 18:29:56 +0200

    Initial commit

---
+++ main/module/Album/autoload_classmap.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: carec
+ * Date: 7/28/2016
+ * Time: 1:05 AM
+ */
+return array();
---
+++ main/module/Album/src/Album/Controller/AlbumController.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: carec
+ * Date: 7/28/2016
+ * Time: 1:34 AM
+ */
+namespace Album\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class AlbumController extends AbstractActionController
+{
+    protected $albumTable;
+
+    public function indexAction()
+    {
+        return new ViewModel(array(
+            'albums' => $this->getAlbumTable()->fetchAll(),
+        ));
+    }
+
+    public function addAction()
+    {
+    }
+
+    public function editAction()
+    {
+    }
+
+    public function deleteAction()
+    {
+    }
+
+    // module/Album/src/Album/Controller/AlbumController.php:
+    public function getAlbumTable()
+    {
+        if (!$this->albumTable) {
+            $sm = $this->getServiceLocator();
+            $this->albumTable = $sm->get('Album\Model\AlbumTable');
+        }
+        return $this->albumTable;
+    }
+}
